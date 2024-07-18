18822e1a89bdc87663a6671084e4b9050dae9d22
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-07-19 02:40:30 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,6 @@
+import NextIcon from "./assets/next-icon.png";
+import FlaskIcon from "./assets/flask-icon.png";
+import MySqlIcon from "./assets/mysql-icon.png";
+import GithubIcon from "./assets/github-icon.png";
+
+export { NextIcon, FlaskIcon, MySqlIcon, GithubIcon };
